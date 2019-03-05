17ea9d8c4ddf65edc893a92f06309e1bc20c0cb3
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-05 12:39:03 +0300

    Turn off plane discovery

--- app/src/main/java/com/gpetuhov/android/samplearcoreimages/AugmentedImageFragment.kt
+++ app/src/main/java/com/gpetuhov/android/samplearcoreimages/AugmentedImageFragment.kt
@@ -1,6 +1,20 @@
 package com.gpetuhov.android.samplearcoreimages
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.google.ar.sceneform.ux.ArFragment
 
 class AugmentedImageFragment : ArFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+
+        // Turn off the plane discovery since we're only looking for images
+        planeDiscoveryController.hide()
+        planeDiscoveryController.setInstructionView(null)
+        arSceneView.planeRenderer.isEnabled = false
+        return view
+    }
 }

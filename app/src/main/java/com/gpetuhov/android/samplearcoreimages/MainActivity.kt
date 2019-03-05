affa226fc6f78be4b29d052bca846e461038098c
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-05 13:30:27 +0300

    Add image tracking

--- app/src/main/java/com/gpetuhov/android/samplearcoreimages/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplearcoreimages/MainActivity.kt
@@ -6,6 +6,12 @@ import android.content.Context
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import com.google.ar.core.AugmentedImage
+import com.google.ar.core.TrackingState
+import com.google.ar.sceneform.FrameTime
+import com.google.ar.sceneform.ux.ArFragment
 import com.pawegio.kandroid.toast
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +19,9 @@ class MainActivity : AppCompatActivity() {
     companion object {
         const val MIN_OPENGL_VERSION = 3.0
     }
+
+    private var arFragment: ArFragment? = null
+    private var fitToScanView: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +31,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         setContentView(R.layout.activity_main)
+
+        arFragment = supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment
+        fitToScanView = findViewById(R.id.image_view_fit_to_scan)
+
+        arFragment?.arSceneView?.scene?.addOnUpdateListener(::onUpdateFrame)
     }
 
     /**
@@ -50,5 +64,53 @@ class MainActivity : AppCompatActivity() {
         }
 
         return true
+    }
+
+    /**
+     * Registered with the Sceneform Scene object, this method is called at the start of each frame.
+     *
+     * @param frameTime - time since last frame.
+     */
+    private fun onUpdateFrame(frameTime: FrameTime) {
+        val frame = arFragment?.arSceneView?.arFrame
+
+        // If there is no frame or ARCore is not tracking yet, just return.
+        if (frame == null || frame.camera.trackingState != TrackingState.TRACKING) {
+            return
+        }
+
+        val updatedAugmentedImages = frame.getUpdatedTrackables(AugmentedImage::class.java)
+
+        for (augmentedImage in updatedAugmentedImages) {
+            val trackingState = augmentedImage.trackingState
+
+            if (trackingState != null) {
+                when (trackingState) {
+                    TrackingState.PAUSED -> {
+                        // When an image is in PAUSED state, but the camera is not PAUSED, it has been detected,
+                        // but not yet tracked.
+                        // Do nothing
+                    }
+
+                    TrackingState.TRACKING -> {
+                        fitToScanView?.visibility = View.GONE
+
+                        // TODO: implement
+                        // Create a new anchor for newly found images.
+//                        if (!augmentedImageMap.containsKey(augmentedImage)) {
+//                            val node = AugmentedImageNode(this)
+//                            node.setImage(augmentedImage)
+//                            augmentedImageMap.put(augmentedImage, node)
+//                            arFragment?.arSceneView?.scene?.addChild(node)
+//                        }
+                    }
+
+                    TrackingState.STOPPED -> {
+                        // TODO: implement
+//                        augmentedImageMap.remove(augmentedImage)
+                    }
+                }
+            }
+        }
     }
 }
